84c07a14dc5f90d03de0260167a354acde383dd0
Eakkaphop pengtham <34792704+DvlpYato@users.noreply.github.com>
2021-11-20 20:09:58 +0700

    Update connect.php

--- Config/connect.php
+++ Config/connect.php
@@ -1,9 +1,9 @@
 <?php
 $info = array(
-    'host' => 'ns2.bestthaihost.com:3306',
-    'user' => 'lamphun_admin',
-    'password' => 'lkm8888',
-    'dbname' => 'lamphun_sinopharm01'
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'dbname' => ''
 );
 $conn = mysqli_connect($info['host'], $info['user'], $info['password'], $info['dbname']) or die('Error connection database!');
 mysqli_set_charset($conn, 'utf8');
